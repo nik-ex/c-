d24931774433cd96796d365fd73cd733e80cea70
nik <nik-tepl@yandex.ru>
2023-12-07 17:06:03 +0400

    вторая задача

---
+++ Desktop/Новая папка/4 lekci9/2 задача/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write($"{array[i],4}");
+}
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]%2==0)
+        count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count);
